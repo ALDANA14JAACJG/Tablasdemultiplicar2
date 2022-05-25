2fcfda7d4d54c459565314b4ca1fb86a9a4493c1
Jorge Aldana <jorge.acarrillo@alumnos.udg.mx>
2022-05-24 23:03:00 -0500

    ;-;

--- app/src/main/java/com/aldana/jorge/tablasdemultiplicar/ResultadoActivity.kt
+++ app/src/main/java/com/aldana/jorge/tablasdemultiplicar/ResultadoActivity.kt
@@ -1,25 +1,35 @@
 package com.aldana.jorge.tablasdemultiplicar
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
 
 class ResultadoActivity : AppCompatActivity() {
     lateinit var animView:LottieAnimationView
+    lateinit var btnRegresar:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
         animView = findViewById(R.id.animation_view)
 
-        animView.setAnimation(R.raw.documentchecktrue)/*
+
         val extras = intent.extras
         val respuestaCorrecta = extras!!.getBoolean("respuesta")
         if(respuestaCorrecta){
-            animView.setAnimation(R.raw.documentchecktrue)
+            animView.setAnimation(R.raw.correcto)
         }else{
-            animView.setAnimation(R.raw.rejectdocumentfiles)
-        }*/
+            animView.setAnimation(R.raw.incorrecta)
+        }
+
+        btnRegresar = findViewById(R.id.btnRegresar)
+
+        btnRegresar .setOnClickListener{
+            val intentPracticar = Intent(this,PracticaActivity2::class.java)
+            startActivity(intentPracticar)
     }
+}
 }
